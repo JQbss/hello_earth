3140be55e0d28ba5107e7ee5c1d19e56719a61a2
Jakub Krasucki <jakub.krasucki@appchance.com>
2022-10-30 21:49:22 +0100

    Unit tests

---
+++ test/unit/repositories/main_missions_repository_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_earth/networking/models/base_response.dart';
+import 'package:hello_earth/networking/models/main_missions.dart';
+import 'package:hello_earth/repositories/main_missions/network_main_missions_repository.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'main_missions_repository_test.mocks.dart';
+
+class MainMissionsRepositoryTest extends Mock implements NetworkMainMissionsRepository {}
+
+@GenerateMocks([MainMissionsRepositoryTest])
+Future<void> main() async {
+  late MainMissionsRepositoryTest mainMissionsRepository;
+  setUpAll(() {
+    mainMissionsRepository = MockMainMissionsRepositoryTest();
+  });
+  group('Main missions Repository tests', () {
+    final BaseResponse<MainMissions> mainMissions = BaseResponse.fromJson({});
+    test('Get all main missions', () async {
+      when(mainMissionsRepository.getMainMissions()).thenAnswer((_) async {
+        return mainMissions;
+      });
+
+      final result = await mainMissionsRepository.getMainMissions();
+      expect(result, mainMissions);
+      expect(result.data, isA<MainMissions>());
+    });
+  });
+}
